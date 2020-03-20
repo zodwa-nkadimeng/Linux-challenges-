c8f93663c028ef68773288d4c17fa7e5883f3b95
zodwa-nkadimeng <nkadimengntombizodwa@gmail.com>
2020-03-20 13:37:14 +0200

    initial linx

---
+++ task 4-8.sh
@@ -0,0 +1,37 @@
+locate umuzi 
+ locate umuzi > search_result.md
+
+task 5 
+ cd Documents
+ touch pad.md
+cd Desktop
+touch work 
+cp pad.md pad_copy.md
+locate updatedb
+cd-
+locate pad_copy.md
+
+
+task 6 
+
+find ~ -name \*pdf 
+find ~ -name \*pdf > fileofchoice.md
+find . -mtime -1 -print
+
+
+TASk 7 
+nano my_bio.md 
+mkdir my_files 
+mv my_bio ~/../../my_files
+sudo 
+Task 8 
+sudo apt update
+sudo apt upgrade
+sudo apt install
+sudo apt install tree
+
+
+
+
+
+
